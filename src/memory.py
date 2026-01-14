4d5c7c1a8c3ae8385b3a3e15b9f02e9624b5ca4d
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2026-01-14 19:53:03 +0800

    feat: attach the logic of the Privileged ISA to the MEM stage

--- src/memory.py
+++ src/memory.py
@@ -13,6 +13,8 @@ class MemoryAccess(Module):
                 # - ALU 指令：计算结果
                 # - JAL/JALR 指令：PC+4 (由 EX 级 Mux 进来)
                 "alu_result": Port(Bits(32)),
+                # - CSR ALU 计算结果
+                "csr_result": Port(Bits(32)),
             }
         )
         self.name = "MEM"
@@ -23,13 +25,25 @@ class MemoryAccess(Module):
         wb_module: Module,  # 下一级流水线 (writeback.py)
         sram_dout: Array,  # SRAM 的输出端口 (Ref)
         mem_bypass_reg: Array,  # 全局 Bypass 寄存器 (数据)
+        mem_csr_bypass_reg: Array,  # 全局 CSR 旁路寄存器 (数据)
+        flush_all_pc: Array,  # 全局流水线冲刷信号 (Ref)
     ):
         # 1. 弹出并解包，提取需要的控制信号
-        ctrl, alu_result = self.pop_all_ports(False)
+        ctrl, alu_result, csr_result = self.pop_all_ports(False)
         mem_opcode = ctrl.mem_opcode
         mem_width = ctrl.mem_width
         mem_unsigned = ctrl.mem_unsigned
         wb_ctrl = wb_ctrl_signals.view(ctrl.wb_ctrl)
+
+        # 2. 判定是否 Flush 并确定相关使能信号
+        flush_if = flush_all_pc[0] != Bits(32)(0)
+        mem_opcode = flush_if.select(MemOp.NONE, mem_opcode)
+        MEM_result_rd = flush_if.select(Bits(5)(0), wb_ctrl.rd_addr)
+        MEM_result_halt_if = flush_if.select(Bits(1)(0), wb_ctrl.halt_if)
+        MEM_result_is_mret = flush_if.select(Bits(1)(0), wb_ctrl.is_MRET)
+        MEM_exception_valid = flush_if.select(Bits(1)(0), wb_ctrl.Exception_Valid)
+        MEM_result_csr_waddr = flush_if.select(Bits(12)(0), wb_ctrl.csr_waddr)
+        MEM_result_csr_wdata = flush_if.select(Bits(32)(0), csr_result)
 
         with Condition(mem_opcode == MemOp.NONE):
             log("MEM: OP NONE.")
@@ -53,19 +67,12 @@ class MemoryAccess(Module):
         # 2. SRAM 数据加工 (Data Aligner)
         # 读取 SRAM 原始数据 (32-bit)
         raw_mem = sram_dout[0].bitcast(Bits(32))
-
         # 二分选择半字 (16-bit Candidates)
-        # 根据 alu_result[1:1] (地址第1位) 选择高16位还是低16位
-        # 0 -> 低16位 [15:0]
-        # 1 -> 高16位 [31:16]
+        # alu_result[1:1]: 0/1 -> 低/高16位
         half_selected = alu_result[1:1].select(raw_mem[16:31], raw_mem[0:15])
-
         # 二分选择字节 (8-bit Candidates)
-        # 在刚才选出的半字基础上，根据 alu_result[0:0] (地址第0位) 选择高8位还是低8位
-        # 0 -> 低8位
-        # 1 -> 高8位
+        # alu_result[0:0]: 0/1-> 低/高8位
         byte_selected = alu_result[0:0].select(half_selected[8:15], half_selected[0:7])
-
         # 统一处理符号位
         # 对于 Byte：如果是无符号，填充0；否则填充最高位(第7位)
         pad_bit_8 = mem_unsigned.select(Bits(1)(0), byte_selected[7:7])
@@ -73,16 +80,13 @@ class MemoryAccess(Module):
         padding_8 = pad_bit_8.select(Bits(24)(0xFFFFFF), Bits(24)(0))
         # 拼接
         byte_extended = concat(padding_8, byte_selected)
-
         # 对于 Half：如果是无符号，填充0；否则填充最高位(第15位)
         pad_bit_16 = mem_unsigned.select(Bits(1)(0), half_selected[15:15])
         # 生成 16 位的填充掩码
         padding_16 = pad_bit_16.select(Bits(16)(0xFFFF), Bits(16)(0))
         # 拼接
         half_extended = concat(padding_16, half_selected)
-
-        # 根据位宽指令选择最终结果
-        # 使用 mem_width 作为选择信号 (独热码)
+        # 根据位宽指令选择最终结果，使用 mem_width 作为选择信号 (独热码)
         processed_mem_result = mem_width.select1hot(
             byte_extended,  # 对应 MemWidth.BYTE
             half_extended,  # 对应 MemWidth.HALF
@@ -90,25 +94,39 @@ class MemoryAccess(Module):
         )
 
         # 3. 最终数据选择 (Final Mux)
-        # 如果是 Load 指令，用加工后的内存数据
-        # 否则 (ALU运算/JAL/LUI)，用 EX 传下来的 alu_result
+        # 如果是 Load 指令，用加工后的内存数据；否则用 EX 传下来的 alu_result
         is_load = mem_opcode == MemOp.LOAD  # 检查是否为 Load 指令
         is_store = mem_opcode == MemOp.STORE  # 检查是否为 Store 指令
-        final_data = is_load.select(processed_mem_result, alu_result)
+        MEM_result_wdata = is_load.select(processed_mem_result, alu_result)
 
         # 4. 输出驱动 (Output Driver)
         # 驱动级间 Bypass 寄存器
         # 注意：如果当前是气泡 (rd=0)，写入 0 也是安全的
-        mem_bypass_reg[0] = final_data
+        mem_bypass_reg[0] <= MEM_result_wdata
+        mem_csr_bypass_reg[0] <= MEM_result_csr_wdata
+        log("MEM: Bypass <= 0x{:x}", MEM_result_wdata)
 
-        log("MEM: Bypass <= 0x{:x}", final_data)
+        # 5. 捆绑信号，驱动下一级 WB
+        MEM_pc = wb_ctrl.PC
+        MEM_result_wb_ctrl = wb_ctrl_signals.bundle(
+            rd_addr=MEM_result_rd,
+            halt_if=MEM_result_halt_if,
+            is_MRET=MEM_result_is_mret,
+            Exception_Valid=MEM_exception_valid,
+            Exception_Code=wb_ctrl.Exception_Code,
+            Exception_Val=wb_ctrl.Exception_Val,
+            PC=MEM_pc,
+            csr_waddr=MEM_result_csr_waddr,
+        )
+        wb_call = wb_module.async_called(
+            ctrl=MEM_result_wb_ctrl,
+            wdata=MEM_result_wdata,
+            csr_wdata=MEM_result_csr_wdata,
+        )
+        wb_call.bind.set_fifo_depth(ctrl=1, wdata=1, csr_wdata=1)
 
-        # 驱动下一级 WB (Main Channel)
-        wb_call = wb_module.async_called(ctrl=wb_ctrl, wdata=final_data)
-        wb_call.bind.set_fifo_depth(ctrl=1, wdata=1)
-
-        # 引脚暴露 (供 HazardUnit 使用)
-        return wb_ctrl.rd_addr, is_store
+        # 引脚暴露 (供 HazardUnit 与 CSRsUnit 使用)
+        return MEM_result_rd, MEM_result_csr_waddr, MEM_pc, is_store
 
 
 class SingleMemory(Downstream):
@@ -121,13 +139,15 @@ class SingleMemory(Downstream):
         self,
         # --- 来自 IF 阶段的接口 ---
         if_addr: Value,  # 取指地址 (PC)
-        # --- 来自 EX 阶段的接口 (优先) ---
+        # --- 来自 EX 阶段的接口 ---
         mem_addr: Value,  # 访存地址 (ALU Result)
         re: Value,  # 读使能 (Load)
         we: Value,  # 写使能 (Store)
         wdata: Value,  # 写数据 (Store Value)
         width: Value,  # 访存宽度 (Byte/Half/Word)
         sram: SRAM,  # 物理 SRAM 资源引用
+        # --- 来自 CSRs Unit 的接口 ---
+        flush_all_signal: Value,  # 全局流水线冲刷信号
     ):
         # 0. 使用 optional 弹出端口
         if_addr_val = if_addr.optional(Bits(32)(0))
@@ -136,6 +156,7 @@ class SingleMemory(Downstream):
         we_val = we.optional(Bits(1)(0))
         wdata_val = wdata.optional(Bits(32)(0))
         width_val = width.optional(Bits(3)(1))
+        flush_if = flush_all_signal.optional(Bits(1)(0))
 
         # 1. 定义状态寄存器
         # 0: IDLE/READ Phase; 1: WRITE Phase
@@ -147,7 +168,7 @@ class SingleMemory(Downstream):
 
         # 2. 状态迁移逻辑
         # store_state 更新
-        store_reg_refresh = we_val & ~store_state[0]
+        store_reg_refresh = we_val & ~store_state[0] & ~flush_if
         store_state[0] <= store_reg_refresh.select(Bits(1)(1), Bits(1)(0))
         # 地址寄存器更新
         store_addr[0] <= store_reg_refresh.select(mem_addr_val, Bits(32)(0))
@@ -158,7 +179,7 @@ class SingleMemory(Downstream):
 
         # 3. SRAM 输入计算
         # 读使能/写使能确定
-        SRAM_we = store_state[0]
+        SRAM_we = store_state[0] & ~flush_if
         SRAM_re = ~store_state[0]
         # 地址计算与仲裁
         final_mem_addr = store_state[0].select(store_addr[0], mem_addr_val)
